936da6e4e86fd64b2f0698e1c51cc0c3765d71a3
Михаил <mikhail-gankin@yandex.ru>
2020-09-24 00:10:54 +0300

    1

--- js/main.js
+++ js/main.js
@@ -15,21 +15,6 @@ const warm = document.querySelector('.nav__link--warm');
 const cold = document.querySelector('.nav__link--cold');
 const warmMenu = document.querySelector('.warm-menu');
 const coldMenu = document.querySelector('.cold-menu');
-const footerNav = document.querySelector('.footer__nav');
-const captions = document.querySelectorAll('.footer__list h3');
-const lists = document.querySelector('.footer__nav div');
-
-console.log(captions);
-
-const footerNavChanger = () => {
-  let list = document.createElement('ul');
-  for (let i = 0; i < captions.length; i++) {
-    list.appendChild(captions[i]);
-    footerNav.appendChild(list);
-  }
-  lists.remove();
-}
-footerNavChanger();
 
 infoItemEmail.addEventListener('click', () => {
   mailPopup.classList.add('opened');
